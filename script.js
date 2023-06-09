20346cf440d296bb947ade1bb42ea042f761de8c
Downey03 <44365074+Downey03@users.noreply.github.com>
2023-06-09 23:32:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team = players;
-let team1 = {
-	name: "John Doe",
-	age: 80
-}
+let team1 = JSON.parse(JSON.stringify(players));
+let cap1 = JSON.parse(JSON.stringify(person));
 window.players = players;
 window.person = person;
 window.team = team;
